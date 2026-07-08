2f9072ef440875253be68342a1f49a8b7551b75f
talhacodespro <talha563280@gmail.com>
2026-07-09 01:16:41 +0600

    test modul removed

--- src/app/modules/testModule/test.interface.ts
+++
@@ -1,5 +0,0 @@
-export type TTest = {
-	test: string;
-	test2: number;
-	tests: string[];
-};
--- src/app/modules/testModule/test.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import TestController from "./test.controller";
-const router = express.Router();
-
-router.get("/test", TestController.getTests);
-
-
-export const TestRoutes = router;
