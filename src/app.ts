48be98a42b3ec7246a2c0c1146756310af89b0a5
talhacodespro <talha563280@gmail.com>
2026-07-14 00:00:14 +0600

    cors methods chagne

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: 'https://sure-fly-backend.vercel.app/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
 );
